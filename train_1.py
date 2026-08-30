286fc53f3af279fff746b799014956c9aed3ae7d
selincyr <134168579+selincyr@users.noreply.github.com>
2026-08-30 22:12:58 +0300

    Implement PPO training for Helicopter environment

---
+++ train_1.py
@@ -0,0 +1,33 @@
+from stable_baselines3 import PPO
+from helicopter_env_1 import HelicopterEnv
+
+
+def main():
+
+    env = HelicopterEnv()
+
+    model = PPO(
+        "MlpPolicy",
+        env,
+        verbose=1,
+        learning_rate=3e-4,
+        gamma=0.99,
+        n_steps=1024,
+        batch_size=64
+    )
+
+    model.learn(
+        total_timesteps=100_000
+    )
+
+    model.save(
+        "ppo_ah1s_task1"
+    )
+
+    env.close()
+
+    print("Task 1 training tamamlandı.")
+
+
+if __name__ == "__main__":
+    main()
