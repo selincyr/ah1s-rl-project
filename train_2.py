ba92293904864c7a8798e2600f71f68b1f4bc2a6
selincyr <134168579+selincyr@users.noreply.github.com>
2026-09-01 14:12:57 +0300

    Update train_2.py

--- train_2.py
+++ train_2.py
@@ -51,11 +51,11 @@ def main():
     # =====================================================
 
     model.save(
-        "ppo_ah1s_1000ft_v6"
+        "ppo_ah1s_1000ft_v7"
     )
 
     print(
-        "Model kaydedildi: ppo_ah1s_1000ft_v6.zip"
+        "Model kaydedildi: ppo_ah1s_1000ft_v7.zip"
     )
 
     env.close()
