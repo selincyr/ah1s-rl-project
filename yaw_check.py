b093b3b0922be098a29345fa22d66455558e5390
selincyr <134168579+selincyr@users.noreply.github.com>
2026-09-01 11:21:39 +0300

    Update yaw_check.py

--- yaw_check.py
+++ yaw_check.py
@@ -15,12 +15,12 @@ def create_fdm():
   return fdm
 
 def run_test(name,rudder_value):
-  print()
+  #print()
 
-  print("===============================")
-  print(name)
-  print("Rudder:", rudder_value)
-  print("===============================")
+ # print("===============================")
+ # print(name)
+ # print("Rudder:", rudder_value)
+ # print("===============================")
   fdm = create_fdm()
 
 #hover civarı temel kontroller
@@ -33,23 +33,27 @@ def run_test(name,rudder_value):
   for step in range(500):
     for _ in range(10):
       fdm.run()
-    if step % 50 == 0:
-      heading = fdm["attitude/heading-true-rad"]
-      yaw_rate = fdm["velocities/r-rad_sec"]
-      altitude = fdm["position/h-agl-ft"]
-      print(
-        f"Step {step:3d} |" f"Heading {heading:7.3f} |" f"Yaw rate {yaw_rate:7.3f} |" f"Altitude {altitude:7.2f}")
+   # if step % 50 == 0:
+    #  heading = fdm["attitude/heading-true-rad"]
+     # yaw_rate = fdm["velocities/r-rad_sec"]
+      #altitude = fdm["position/h-agl-ft"]
+      #print(
+       # f"Step {step:3d} |" f"Heading {heading:7.3f} |" f"Yaw rate {yaw_rate:7.3f} |" f"Altitude {altitude:7.2f}")
     final_heading = fdm["attitude/heading-true-rad"]
+    return(start_heading, final_heading)
+   # print()
+    #print("Start heading:",round(start_heading,3))
+    #print("Final heading:",round(final_heading,3))
+def main():
+  
+    base_start,base_final = run_test("BASE RUDDER",0.386)
+    low_start,low_final =run_test("LOW RUDDER",0.316)
+    high_start,high_final= run_test("HIGH RUDDER",0.456)
     print()
-    print("Start heading:",round(start_heading,3))
-    print("Final heading:",round(final_heading,3))
-def main(): 
-  #base trim rudder
-    run_test("BASE RUDDER",0.386)
-  #RUDDER biraz azaltilmiş
-    run_test("LOW RUDDER",0.316)
-  #RUDER BİRAZ ARTİRİLMİŞ
-    run_test("HIGH RUDDER",0.456)
+    print("=============YAW CHECK OZET===============")
+    print("BASE:",round(base_start,3),"->",round(base_final,3))
+    print("LOW:",round(low_start,3),"->",round(low_final,3))
+    print("HIGH:",round(high_start,3),"->",round(high_final,3))
 
 
 if __name__ == "__main__":
